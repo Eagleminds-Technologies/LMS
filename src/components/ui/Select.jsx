aa4d7127cbba76e711889092d2130374b3ba8105
eaglemindsdev <118447921+eaglemindsdev@users.noreply.github.com>
2025-05-07 13:26:08 +0530

    Initial commit: SaaS Admin Panel LMS setup

--- src/components/ui/Select.jsx
+++ src/components/ui/Select.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { Check, ChevronDown, Search } from "lucide-react";
 import { cn } from "../../utils/helpers";
-import { Label } from "./Input";
+import { Label } from "./Label";
 
 const Select = SelectPrimitive.Root;
 const SelectGroup = SelectPrimitive.Group;
